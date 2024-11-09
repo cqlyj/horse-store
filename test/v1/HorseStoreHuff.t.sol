1482fb1d2a3d180e818b65b35a7a1e2c71919f72
cqlyj <luoyingjie0721@gmail.com>
2024-11-10 01:38:15 +0800

    refactor HorseStoreHuff to use IHorseStore interface for horseStore initialization

--- test/v1/HorseStoreHuff.t.sol
+++ test/v1/HorseStoreHuff.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {Base_TestV1, HorseStore} from "test/v1/Base_TestV1.t.sol";
+import {Base_TestV1, IHorseStore} from "test/v1/Base_TestV1.t.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
 contract HorseStoreHUff is Base_TestV1 {
@@ -9,7 +9,7 @@ contract HorseStoreHUff is Base_TestV1 {
         "horseStoreV1/HorseStore";
 
     function setUp() public override {
-        horseStore = HorseStore(
+        horseStore = IHorseStore(
             HuffDeployer.config().deploy(HORSE_STORE_HUFF_LOCATION)
         );
     }
